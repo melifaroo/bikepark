774399c4bc08a4e9991a30d1bd2ad3c36c76e79f
denis volgin <den.volgin@gmail.com>
2022-07-24 12:56:05 +0300

    sqlite2mysql converter

---
+++ Sqlite2mysql/MySqlBikeparkContext.cs
@@ -0,0 +1,21 @@
+﻿using Bikepark.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sqlite2mysql
+{
+    public class MySqlBikeparkContext : Bikepark.Data.BikeparkContext
+    {
+        public MySqlBikeparkContext(string connectionString, MySqlServerVersion version) 
+            : base(new DbContextOptionsBuilder<BikeparkContext>()
+                  .UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 28)))
+                  .EnableSensitiveDataLogging()
+                  .Options )
+        {
+        }
+    }
+}
